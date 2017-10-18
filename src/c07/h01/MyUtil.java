fc828207c8598e264ec8db45151be01edfbe46ac
tjwhm <759361109@qq.com>
2017-10-19 00:28:25 +0800

    updated c07

--- src/c07/h01/MyUtil.java
+++ src/c07/h01/MyUtil.java
@@ -6,30 +6,36 @@ package c07.h01;
  */
 public class MyUtil {
     public String getFilename(String fullPathname) {
-        String res = null;
-        for (int i = fullPathname.length() - 1; i > -1; i--) {
+        int i;
+        for (i = fullPathname.length() - 1; i > -1; i--) {
             if ('/' == fullPathname.charAt(i)) {
-                res = fullPathname.substring(i + 1, fullPathname.length());
                 break;
             }
         }
-        return res;
+        return fullPathname.substring(i + 1, fullPathname.length());
     }
 
     public String getStudentID(String fullPathname) {
         String fileName = getFilename(fullPathname);
-        return fileName.substring(0, 10);
+        int end = 0;
+        for (int i = 0; i < fileName.length(); i++) {
+            if (fileName.charAt(i) == '_') {
+                end = i;
+            }
+        }
+        return fileName.substring(0, end);
     }
 
     public String getStudentName(String fullPathname) {
         String fileName = getFilename(fullPathname);
+        String studentID = getStudentID(fullPathname);
         int index = 0;
         for (int i = 0; i < fileName.length(); i++) {
             if ('.' == fileName.charAt(i)) {
                 index = i;
             }
         }
-        return fileName.substring(11, index);
+        return fileName.substring(studentID.length() + 1, index);
     }
 
     public void zipSpace(StringBuffer buf) {
